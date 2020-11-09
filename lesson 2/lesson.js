6717eafa7b29a09016ea5fbef37556c650d52891
Mikita Papok <popok11014@tut.by>
2020-11-09 22:21:43 +0300

    added task 2,3 in lesson.js

--- lesson 2/lesson.js
+++ lesson 2/lesson.js
@@ -1,1 +1,36 @@
-alert('hi!');
+
+//zadanie b
+
+//let a = Symbol();
+//let b = 1;
+//let c = 'hello';
+//let d = null;
+//let e = true;
+//let f = {};
+//let value =d;
+//if(typeof(value) === 'object'){
+//    console.log(typeof(value));
+//} else{alert(typeof(value));};
+let value = function(a){
+    if(typeof(a) === 'object'){
+           console.log(typeof(a));
+        } else{alert(typeof(a));};
+      return;
+}
+let a = function(){};
+value(a);
+
+
+
+//zadanie c
+let age = Number(prompt("Укажите сво возраст"));
+let minor = 'Вы несовершеннолетний!';
+let adult = 'Вы совершеннолетний!';
+if(age < 18){
+   alert("поздравляем," + ' вам' + ' ' +age + ' '+ "полных лет");
+    alert (minor);
+} else{
+    alert("поздравляем," + ' вам' + ' ' +age + ' '+ "полных лет");
+ alert(adult);
+};
+
